2c44a726e2abf320e2e4421170ed407a84b46c94
Tabish1511 <khaqantabish@gmail.com>
2025-02-25 16:10:32 +0000

    add downbase to projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,6 +13,42 @@ export const Projects = () => {
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <h3 className="text-xl font-bold mb-2"> DownBase</h3>
+            <p className="text-gray-400 mb-4">
+              <strong>Implementation:</strong> Signup/Signin, upload images (currently 'jpg' compatible) from the nav bar on the left and see all your images 
+              in the home page.<br/>
+            <br/>
+              <strong>Description:</strong> Reliable, 0% downtime images store implemented with AWS / S3 / Cloudfront. 
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["Python", "Django", "AWS", "Docker", "S3", "Cloudfront", "CI/CD"].map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                  "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+              <a
+                href="https://downbase.tabishkhaqan.me/register"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View Project →
+              </a>
+            </div>
+          </div>
+            
+            
+            
+            
+            
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2"> Journalink (Experimental)</h3>
               <p className="text-gray-400 mb-4">
@@ -45,6 +81,7 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+            
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
